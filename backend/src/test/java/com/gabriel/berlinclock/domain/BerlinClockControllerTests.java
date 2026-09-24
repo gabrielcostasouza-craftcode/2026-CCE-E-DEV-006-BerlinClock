ab9faaf90585ed23cee8e55fdc60f982d8ecc4e5
costaga <costaga@contribute.be>
2026-09-24 15:43:23 +0200

    Impl: Red-Test: BerlinClockController - to digital time

--- backend/src/test/java/com/gabriel/berlinclock/domain/BerlinClockControllerTests.java
+++ backend/src/test/java/com/gabriel/berlinclock/domain/BerlinClockControllerTests.java
@@ -47,4 +47,20 @@ public class BerlinClockControllerTests {
                 .andExpect(status().isBadRequest());
     }
 
+
+    @ParameterizedTest(name = "{0} -> {1}")
+    @CsvSource({
+            "YOOOOOOOOOOOOOOOOOOOOOOO,	00:00:00",
+            "ORRRRRRROYYRYYRYYRYYYYYY,	23:59:01",
+            "YRRROROOOYYRYYRYYRYOOOOO,	16:50:00",
+            "ORROOROOOYYRYYRYOOOOYYOO,	11:37:01"
+    })
+    void berlinTimeToDigitalTime(String time, String expectedLights) throws Exception {
+        mockMvc.perform(get("/api/to-digital-time").param("localtime", time))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.digitalTime").value(time))
+                .andExpect(jsonPath("$.berlinTime").value(expectedLights));
+    }
+
+
 }
