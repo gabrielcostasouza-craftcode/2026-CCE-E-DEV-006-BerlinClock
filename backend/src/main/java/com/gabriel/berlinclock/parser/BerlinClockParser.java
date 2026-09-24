e62ac619ea301c20bd8edff5ec8c25b8d0e6a1f9
costaga <costaga@contribute.be>
2026-09-24 11:26:21 +0200

    Impl: Red-Test: Five Minute Rows, Single and Five Hour Rows, Seconds Row. Full time parsing,

--- backend/src/main/java/com/gabriel/berlinclock/parser/BerlinClockParser.java
+++ backend/src/main/java/com/gabriel/berlinclock/parser/BerlinClockParser.java
@@ -8,4 +8,24 @@ public class BerlinClockParser {
         int lightedUpLamps = time.getMinute() % 5; //5 because every 5th min lamp will be on another row.
         return "Y".repeat(lightedUpLamps) + "0".repeat(4-lightedUpLamps);
     }
+
+    public String fiveMinuteRow(LocalTime time) {
+        throw new UnsupportedOperationException("This convertor hasn't been implemented yet");
+    }
+
+    public String singleHourRow(LocalTime time) {
+        throw new UnsupportedOperationException("This convertor hasn't been implemented yet");
+    }
+
+    public String fiveHourRow(LocalTime time) {
+        throw new UnsupportedOperationException("This convertor hasn't been implemented yet");
+    }
+
+    public String secondsLampRow(LocalTime time) {
+        throw new UnsupportedOperationException("This convertor hasn't been implemented yet");
+    }
+
+    public String parseFull(LocalTime time) {
+        throw new UnsupportedOperationException("This convertor hasn't been implemented yet");
+    }
 }
--- backend/src/test/java/com/gabriel/berlinclock/BerlinclockParserTests.java
+++ backend/src/test/java/com/gabriel/berlinclock/BerlinclockParserTests.java
@@ -29,7 +29,66 @@ class BerlinclockParserTests {
     })
     void singleMinuteRowLightsUpEveryMinuteNoMultiplesFive(LocalTime time, String expectedLights) {
         assertThat(bcParser.singleMinuteRow(time).equals(expectedLights));
-
     }
+
+
+    @ParameterizedTest(name = "{0} -> {1}")
+    @CsvSource({
+            "00:00:00,	OOOOOOOOOOO",
+            "23:59:59,	YYRYYRYYRYY",
+            "12:04:00,	OOOOOOOOOOO",
+            "12:23:00,	YYRYOOOOOOO",
+            "12:35:00,	YYRYYRYOOOO"
+    })
+    void fiveMinuteRowLightsUpLampForEveryFiveMinutesAndRedEveryThirdLamp(LocalTime time, String expectedLights) {
+        assertThat(bcParser.fiveMinuteRow(time).equals(expectedLights));
+    }
+
+
+    @ParameterizedTest(name = "{0} -> {1}")
+    @CsvSource({
+            "00:00:00,	OOOO",
+            "23:59:59,	RRRO",
+            "02:04:00,	RROO",
+            "08:23:00,	RRRO",
+            "14:35:00,	RRRR"
+    })
+    void singleHourRowLightsUpEveryMinuteNoMultiplesFive(LocalTime time, String expectedLights) {
+        assertThat(bcParser.singleHourRow(time).equals(expectedLights));
+    }
+
+    @ParameterizedTest(name = "{0} -> {1}")
+    @CsvSource({
+            "00:00:00,	OOOO",
+            "23:59:59,	RRRR",
+            "02:04:00,	OOOO",
+            "08:23:00,	ROOO",
+            "16:35:00,	RRRO"
+    })
+    void fireHourRowLightsUpRedLampEveryFifthHour(LocalTime time, String expectedLights) {
+        assertThat(bcParser.fiveHourRow(time).equals(expectedLights));
+    }
+
+    @ParameterizedTest(name = "{0} -> {1}")
+    @CsvSource({
+            "00:00:00,	Y",
+            "23:59:59,	O"
+    })
+    void lampOnEveryEvenSecond(LocalTime time, String expectedLights) {
+        assertThat(bcParser.secondsLampRow(time).equals(expectedLights));
+    }
+
+    @ParameterizedTest(name = "{0} -> {1}")
+    @CsvSource({
+            "00:00:00,	YOOOOOOOOOOOOOOOOOOOOOOO",
+            "23:59:59,	ORRRRRRROYYRYYRYYRYYYYYY",
+            "16:50:06,	YRRROROOOYYRYYRYYRYOOOOO",
+            "11:37:01,	ORROOROOOYYRYYRYOOOOYYOO"
+    })
+    void fullBerlinClockGetsAllRows(LocalTime time, String expectedLights) {
+        assertThat(bcParser.parseFull(time).equals(expectedLights));
+    }
+
+
 
 }
